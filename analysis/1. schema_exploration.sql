396dee38e20e6ef8b918a20eacc06b9e009c260b
Sampath Moolya <sampath.moolya05wrk@gmail.com>
2026-09-21 19:18:43 +0530

    Create 1. schema_exploration.sql

---
+++ analysis/1. schema_exploration.sql
@@ -0,0 +1,8 @@
+-- Explore all Objects in the Database
+
+SELECT * FROM INFORMATION_SCHEMA.TABLES
+
+-- Explore all columns in the Database
+
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'fact_sales'
