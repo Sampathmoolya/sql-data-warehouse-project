859e3b76e36a22f7e67dbd39e93d75d7044cf203
Sampath Moolya <sampath.moolya05wrk@gmail.com>
2026-09-21 19:17:53 +0530

    Delete analysis/exploration_of_schema.sql

--- analysis/exploration_of_schema.sql
+++
@@ -1,8 +0,0 @@
--- Explore all Objects in the Database
-
-SELECT * FROM INFORMATION_SCHEMA.TABLES
-
--- Explore all columns in the Database
-
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'fact_sales'
